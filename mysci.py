46f76d518cfea4ee8db2bc9bb489b28ee0bae987
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2020-10-28 13:36:04 -0600

    Parsing file

--- mysci.py
+++ mysci.py
@@ -1,7 +1,23 @@
 
+# Initialize my data variable
+data = []
+
 # Read the datafile
 filename = "data/wxobs20170821.txt"
 
 with open(filename, 'r') as datafile:
-    data = datafile.read()
+#   data = datafile.read()
+
+    #read the first 3 lines (header)
+    # underscore is a placeholder variable used instead of i or j
+    for _ in range(3):
+        datafile.readline()
+
+    #read and parse the rest of the file
+    for line in datafile:
+        datum = line.split()    #whitespace   
+#       datum = line.split(',')  comma-separated       
+#       datum = line.split(\t)   tab-separated    
+
+        data.append(datum)
 
