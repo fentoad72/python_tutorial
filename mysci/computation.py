56d2584e81836dbd22dd33fc5c0577d3bd58d52e
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2021-02-10 12:53:04 -0700

    Adding new readdata module

---
+++ mysci/computation.py
@@ -0,0 +1,50 @@
+
+def compute_windchill(t, v):
+    """
+    Compute the windchill factor given the temperature and windspeed
+
+    NOTE: This computation is valid only for temperatures between -45F and 45F
+          and for wind speeds between 3 mph and 60 mph
+
+    Parameters:
+        t: the temperature in Fahrenheit
+        v: the wind speed in miles per hour
+    """
+
+    a = 35.74
+    b = 0.6215
+    c = 35.75
+    d = 0.4275
+
+    v16 = v**0.16
+
+    wci = a + (b*t) - (c*v16) + (d*t*v16)
+    return wci
+
+
+def compute_heatindex(t, hum):
+    """
+    Compute the heat index given the temperature and humidity
+
+    Parameters:
+        t: the temperature of units in F (float)
+        hum:
+    """
+
+    a = -42.379
+    b = 2.04901523
+    c = 10.14333127
+    d = 0.22475541
+    e = 0.00683783
+    f = 0.0481717
+    g = 0.00122874
+    h = 0.00085282
+    i = 0.00000199
+
+    rh = hum/100.
+
+    hi = (a + (b * t) + (c * rh) + (d * t * rh) + (e * t**2) +
+        (f * rh ** 2) + (g * t**2 * rh) + (h * t * rh**2) +
+        (i * t**2 * rh**2))
+
+    return hi
--- readdata.py
+++ readdata.py
@@ -1,30 +1,30 @@
-
 def read_data(columns, types={}, filename="data/wxobs20170821.txt"):
-    """
-    Read data from CU Boulder Weather Station data file
+   """
+   Read data from CU Boulder Weather Station data file
 
-    Parameters:
-        columns: A dictionary of column names mapping to column indices
-        types: A dictionary of column names mapping to the types to which to convert each column of data
-        filename: A string path pointing to the CU Boulder Weather Station data file
-    """
+   Parameters:
+      columns: A dictionary of column names mapping to column indices
+      types: A dictionary of column names mapping to types to which
+         to convert each column of data
+      filename: The string path pointing to the CU Boulder Weather
+            Station data file
+   """
 
     # Initialize my data variable
     data = {}
     for column in columns:
         data[column] = []
 
-    # Read the datafile
+# Read and parse the data file
     with open(filename, 'r') as datafile:
 
-        #Read the first 3 lines (header)
-        # underscore is a placeholder variable used instead of i or j
+   # Read the first three lines (header)
         for _ in range(3):
             datafile.readline()
 
-        #read and parse the rest of the file
+   # Read and parse the rest of the file
         for line in datafile:
-            split_line = line.split()    #whitespace
+            split_line = line.split()
             for column in columns:
                 i = columns[column]
                 t = types.get(column, str)
