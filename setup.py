c11978d08c30da9e1418f5f1e35bc279cf36c3fd
Todd Arbetter <37349300+fentoad72@users.noreply.github.com>
2021-02-10 13:46:49 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Todd Arbetter",
+    author_email="arbetter@ucar.edu",
+    packages=["mysci"],
+    install_requires=[])
